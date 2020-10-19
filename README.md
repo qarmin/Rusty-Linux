2221cd49c54257a009ae540b7e3c349708d6250e
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-10-19 12:07:07 +0200

    Add basic version of script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Rusty-Linux
+# Rusty Linux
 Simple script to install most basic Rust apps as alternative to default one
+## Install apps
+To install apps you need just download this script and run
+```
+wget https://raw.githubusercontent.com/qarmin/Rusty-Linux/main/README.md
+chmod +x 
+./
+```
---
+++ RustyLinux.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Simple bash script to compile and install basics Rust tools which are replacement to commonly used Linux tools
+# To upgrade apps, just run this script
+
+# Update Rust
+rustup update
+
+# exa - ls
+cargo install exa
+## lsd
+#cargo install lsd
+
+# sd - sed
+cargo install sd
+
+# amp - vim
+cargo install amp
+
+# coreutils uutils - coreutils
+cargo install coreutils
+
+# bat - cat
+cargo install bat
+
+# ripgrep - grep
+cargo install ripgrep
+## ripgrep-all - grep
+# cargo install ripgrep
+
+# dust - du
+cargo install du-dust
+
+# fd - find
+cargo install fd-find
+
+# lolcate - locate
+cargo install lolcate-rs
+
+# websocat - curl
+cargo install websocat
+
+# procs - ps
+cargo install procs
+
+# bottom - top
+cargo install bottom
+
+# choose - cut/awk
+cargo install choose
+
+# zenith - htop
+cargo install --git https://github.com/bvaisvil/zenith.git
+
+# alacritty - terminal
+cargo install alacritty
+
+# tokei - cloc
+cargo install tokei
+
+# process viewer - system monitor
+cargo install process_viewer
+
+# gxi - text editor
+# Needs GTK 3
+cargo install --git https://github.com/bvinc/gxi.git
+
+# popsicle - Etcher
+cargo install --git https://github.com/pop-os/popsicle.git popsicle_gtk
+
+# Czkawka - FSlint
+# Needs GTK 3.22+
+cargo install czkawka_gui
+
